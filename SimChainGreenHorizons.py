71c1b032c07cf9ebc387021d835dd76a700ec503
MARTZ Yannick <yannick.martz@scalian.com>
2024-01-12 13:19:59 +0000

    Upload New File

---
+++ SimChainGreenHorizons.py
@@ -0,0 +1,62 @@
+
+import os
+from greytheory import GreyTheory
+grey = GreyTheory()
+import line_production
+from SupplyChainOptimization import run_supply_chain_optimization
+
+from data_tools import plot_surface, visualize_stock_levels, plot_co2_emissions, round_to_nearest_significant
+
+
+def main_function():
+    # Your main function logic
+
+    absolute_path = os.path.dirname(__file__)
+
+    # Example usage of the plotting functions from data_tools.py
+    
+    data, total_factory_lines = line_production.get_data()
+    number_of_seat = data['Total Seats made'][1][-1] 
+    rounded_value = round_to_nearest_significant(number_of_seat)
+    ytick_param = rounded_value / (rounded_value/(rounded_value/10))
+
+
+    # visualize_stock_levels(data, ytick_param)
+
+
+    fig_opti, fig2_opti, source, target, value, node_labels = run_supply_chain_optimization()
+
+    # print(node_labels)
+
+    # Show the figure
+    # fig_opti.show()
+
+
+    # Show the figure
+    # fig2_opti.show()
+
+    # # # Assuming 'final_report.csv' is generated and saved in the 'data' directory within the same directory as your script
+    # # csv_file_path = os.path.join(absolute_path, 'data/final_report.csv')
+
+    # # Call the function to plot the CO2 emissions
+    # plot_co2_emissions(csv_file_path)
+
+    forecasted_data = []
+    previous_forecasted_results = []
+    curve =[]
+    original_data = []
+
+
+    gm11 = grey.gm11
+
+    original_data=data['Total Seats made'][1]
+    print(original_data)
+
+
+if __name__ == '__main__':
+
+    main_function()
+    
+   
+
+   
